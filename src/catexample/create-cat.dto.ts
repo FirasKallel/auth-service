0adb2cf5ff3fbf976cdb8f0a94af0536bd73e031
Firas Kallel <kallel.kun@gmail.com>
2021-01-29 14:27:13 +0100

    ➕ Add class validator

--- src/catexample/create-cat.dto.ts
+++ src/catexample/create-cat.dto.ts
@@ -1,7 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength } from 'class-validator';
 
 export class CreateCatDto {
   @ApiProperty()
+  @IsString()
+  @MinLength(4)
   name: string;
   @ApiProperty()
   age: number;
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import * as morgan from 'morgan'
+import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('common'));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle(process.env.API_TITLE)
     .setDescription('')
