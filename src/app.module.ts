2c223d96bd488a0432de44920eb89ea1f4fcc372
Firas Kallel <kallel.kun@gmail.com>
2021-01-29 14:20:40 +0100

    ➕ Add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+import { MongooseModule } from '@nestjs/mongoose';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGODB_SERVER +
+        '://' +
+        process.env.MONGODB_USER +
+        ':' +
+        process.env.MONGODB_PASSWORD +
+        '@localhost:' +
+        process.env.MONGODB_PORT,
+    ),
+    CatexampleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
