b31f50c0529dd44a94e2d4b59268f802b271a1b0
Firas Kallel <kallel.kun@gmail.com>
2021-02-06 16:11:48 +0100

    🐛🎨 Fix and improve Professor module

--- src/professor/professor.controller.ts
+++ src/professor/professor.controller.ts
@@ -1,45 +1,49 @@
-import { Body, Controller, Get, Param, Post, Query, Put,Delete} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { ProfessorService } from './professor.service';
 import { get } from 'https';
 import { CreateProfessorDto } from './create-professor.dto';
 import { ok } from 'assert';
 import { ApiTags } from '@nestjs/swagger';
 
-@Controller('professor')
-export class ProfessorController {}
-
 @ApiTags('professor')
 @Controller('professor')
-export class StudentController 
-{   
-    constructor(private professorService: ProfessorService) {}
+export class ProfessorController {
+  constructor(private professorService: ProfessorService) {}
 
-    @Get()
-    async getStudents(){
-        return this.professorService.gets();
-    }
+  @Get()
+  async getProfessors() {
+    return this.professorService.gets();
+  }
 
-    @Get(':cin')
-    async getStudent(@Param('cin') cin: string){
-        return this.professorService.get(cin);
-    }    
+  @Get(':cin')
+  async getProfessor(@Param('cin') cin: string) {
+    return this.professorService.get(cin);
+  }
 
-    @Post()
-    async create(@Body() ProfessorDto: CreateProfessorDto) {
-        const newProf = this.professorService.create(ProfessorDto);
-        return newProf;
-    }
+  @Post()
+  async create(@Body() ProfessorDto: CreateProfessorDto) {
+    const newProf = this.professorService.create(ProfessorDto);
+    return newProf;
+  }
 
-    @Put()
-    async put(@Body() professorDto: CreateProfessorDto ) {
-        const Prof = this.professorService.update(professorDto);
-        return Prof;
-    }
+  @Put()
+  async put(@Body() professorDto: CreateProfessorDto) {
+    const Prof = this.professorService.update(professorDto);
+    return Prof;
+  }
 
-    @Delete(':cin')
-    async deleteStudent(@Param('cin')cin: string){
-        this.professorService.delete(cin);
-        return cin + " deleted";
-    }
-
+  @Delete(':cin')
+  async deleteProfessor(@Param('cin') cin: string) {
+    this.professorService.delete(cin);
+    return cin + ' deleted';
+  }
 }
--- src/professor/professor.module.ts
+++ src/professor/professor.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProfessorService } from './professor.service';
 import { ProfessorController } from './professor.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProfessorSchema } from './professor.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Professor', schema: ProfessorSchema }]),
+  ],
   providers: [ProfessorService],
-  controllers: [ProfessorController]
+  controllers: [ProfessorController],
 })
 export class ProfessorModule {}
