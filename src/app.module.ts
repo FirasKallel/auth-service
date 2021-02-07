19a8f6c88372b8122188dc16009ef4efe7238536
Firas Kallel <kallel.kun@gmail.com>
2021-02-07 17:47:03 +0100

    🔧 Update config files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,40 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import * as dotenv from 'dotenv';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CatexampleModule } from './catexample/catexample.module';
-import { StudentModule } from './student/student.module';
-import { ProfessorModule } from './professor/professor.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { UsersModule } from './users/users.module';
+import { StudentsModule } from './students/students.module';
+import { ProfessorsModule } from './professors/professors.module';
+import { SessionsModule } from './sessions/sessions.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ConferencesModule } from './conferences/conferences.module';
+import * as dotenv from 'dotenv';
+import { AcademicYearModule } from './academic-year/academic-year.module';
+import { EnterprisesModule } from './enterprises/enterprises.module';
+import { MailingModule } from './mailing/mailing.module';
+import { APP_GUARD } from "@nestjs/core";
+import { RolesGuard } from "./authentication/guards/roles.guard";
 
 dotenv.config();
-
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      process.env.MONGODB_SERVER +
-        '://' +
-        process.env.MONGODB_USER +
-        ':' +
-        process.env.MONGODB_PASSWORD +
-        '@localhost:' +
-        process.env.MONGODB_PORT,
-    ),
-    CatexampleModule,
-    StudentModule,
-    ProfessorModule,
-    AuthModule,
+    MongooseModule.forRoot(process.env.DATABASE_URI_DEV),
+    AuthenticationModule,
+    UsersModule,
+    StudentsModule,
+    ProfessorsModule,
+    SessionsModule,
+    ProjectsModule,
+    ConferencesModule,
+    AcademicYearModule,
+    EnterprisesModule,
+    MailingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-import * as morgan from 'morgan';
-import { ValidationPipe } from '@nestjs/common';
-
-dotenv.config();
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationErrorFilter } from './filters/validation-error.filter';
+import { RolesGuard } from './authentication/guards/roles.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(morgan('common'));
   app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
+    new ValidationPipe({ errorHttpStatusCode: HttpStatus.BAD_REQUEST }),
   );
+  app.useGlobalFilters(new ValidationErrorFilter());
+
   const config = new DocumentBuilder()
     .setTitle(process.env.API_TITLE)
     .setDescription('')
@@ -23,7 +19,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(process.env.SWAGGER_URI, app, document);
-  app.enableCors({origin: ['http://localhost:4200']})
-  await app.listen(process.env.API_PORT);
+  app.enableCors();
+  await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
